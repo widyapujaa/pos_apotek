dfe3effe820ec82f55b7e45ea1ad7a9c4215be0c
ayetdev <ayetnugraha80@gmail.com>
2026-07-08 21:59:47 +0800

    menambahkan crud obat & supplier, dan revisi class obat supplier

--- class/obat.php
+++ class/obat.php
@@ -9,13 +9,22 @@ class Obat {
     }
 
     public function getAllObat() {
-        $query = "SELECT * FROM $this->table";
+        $query = "SELECT * FROM $this->table INNER JOIN supplier USING (id_supplier)";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
-        $result = $stmt->get_result() ->fetch_assoc();
+        $result = $stmt->get_result() ->fetch_all(MYSQLI_ASSOC);
         return $result;
     }
-    public function createObat($id_obat, $id_supplier, $nama_obat, $kategori_obat, $stok_obat, $harga_obat) {
+    public function getObatById($id_obat) {
+        $query = "SELECT * FROM $this->table WHERE id_obat = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("s", $id_obat);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_assoc();
+        return $result;
+    }
+    public function createObat($id_supplier, $nama_obat, $kategori_obat, $stok_obat, $harga_obat) {
+        $id_obat = $this->db->generateId($this->table, 'id_obat', 'OB');
         $query = "INSERT INTO $this->table (id_obat, id_supplier, nama_obat, kategori_obat, stok_obat, harga_obat) VALUES (?, ?, ?, ?, ?, ?)";
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param("ssssis", $id_obat, $id_supplier, $nama_obat, $kategori_obat, $stok_obat, $harga_obat);
--- class/supplier.php
+++ class/supplier.php
@@ -12,10 +12,19 @@ class Supplier {
         $query = "SELECT * FROM $this->table";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
-        $result = $stmt->get_result() ->fetch_assoc();
+        $result = $stmt->get_result() ->fetch_all(MYSQLI_ASSOC);
         return $result;
     }
-    public function createSupplier($id_supplier, $nama_perusahaan, $no_telepon, $alamat) {
+    public function getSupplierById($id_supplier) {
+        $query = "SELECT * FROM $this->table WHERE id_supplier = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("s", $id_supplier);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_assoc();
+        return $result;
+    }
+    public function createSupplier($nama_perusahaan, $no_telepon, $alamat) {
+        $id_supplier = $this->db->generateId($this->table, 'id_supplier', 'SP');
         $query = "INSERT INTO $this->table (id_supplier, nama_perusahaan, no_telepon, alamat) VALUES (?, ?, ?, ?)";
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param("ssss", $id_supplier, $nama_perusahaan, $no_telepon, $alamat);
@@ -33,4 +42,11 @@ class Supplier {
         $stmt->bind_param("s", $id_supplier);
         return $stmt->execute();
     }
+    public function cekRelasiObat($id_supplier){
+    $query = "SELECT id_supplier FROM obat WHERE id_supplier = ?";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bind_param("s", $id_supplier);
+    $stmt->execute();
+    return $stmt->get_result()->num_rows;
+}
 }
