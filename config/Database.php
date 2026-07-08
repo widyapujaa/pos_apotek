929de13938937c7fb1c1943593e29216c8ce0ef4
widya puja <thelanang2020@gmail.com>
2026-07-08 21:54:22 +0800

    Memberbarui config database

--- config/Database.php
+++ config/Database.php
@@ -19,5 +19,21 @@ class Database {
     public function getConnection() {
         return $this->conn;
     }
+    
+    public function generateId($table,$field,$id_dpn){
+        $query = "SELECT $field FROM $table ORDER BY $field DESC LIMIT 1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_assoc();
+        if(!$result)
+        {
+            return $id_dpn."001";
+        }
+        $idTerakhir = $result[$field];
+        $angka = (int) substr($idTerakhir, strlen($id_dpn));
+        $angka++;
+        return $id_dpn.str_pad($angka,3,"0",STR_PAD_LEFT);
+
+    }
 }
 ?>
