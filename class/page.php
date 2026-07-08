b603d0ed1cac820e41ae41f403c848b65bf79324
widya puja <thelanang2020@gmail.com>
2026-07-08 21:38:43 +0800

    memberbarui  class page

--- class/page.php
+++ class/page.php
@@ -8,33 +8,61 @@ class Page{
                 break;
 
             case "karyawan":
-                include "../page/karyawan.php";
+                include "../page/karyawan/karyawan.php";
+                break;
+            case "add_karyawan":
+                include "../page/karyawan/add_karyawan.php";
+                break;
+            case "edit_karyawan":
+                include "../page/karyawan/edit_karyawan.php";
                 break;
 
             case "user":
                 include "../page/user/user.php";
                 break;
-
             case "add_user":
                 include "../page/user/add_user.php";
                 break;
+            case "edit_user":
+                include "../page/user/edit_user.php";
+                break;
 
             case "supplier":
-                include "page/supplier.php";
+                include "../page/supplier/supplier.php";
+                break;
+            case "add_supplier":
+                include "../page/supplier/add_supplier.php";
+                break;
+            case "edit_supplier":
+                include "../page/supplier/edit_supplier.php";
                 break;
 
             case "pelanggan":
-                include "page/pelanggan.php";
+                include "../page/pelanggan/pelanggan.php";
+                break;
+            case "add_pelanggan":
+                include "../page/pelanggan/add_pelanggan.php";
+                break;
+            case "edit_pelanggan":
+                include "../page/pelanggan/edit_pelanggan.php";
                 break;
 
             case "obat":
-                include "page/obat.php";
+                include "../page/obat/obat.php";
                 break;
-
+            case "add_obat":
+                include "../page/obat/add_obat.php";
+                break;
+            case "edit_obat":
+                include "../page/obat/edit_obat.php";
+                break;
+                
             case "transaksi":
-                include "page/transaksi.php";
+                include "../page/transaksi/transaksi.php";
                 break;
-
+            case "add_transaksi":
+                include "../page/transaksi/add_transaksi.php";
+                break;
             default:
                 include "../page/home.php";
                 break;
